11438d8228042a79415b1b9c87d474d85c0939df
Manuel Esteban Morales Zuarez <79806775+Angstromico@users.noreply.github.com>
2021-07-22 17:43:06 -0400

    Add files via upload

--- conexion.php
+++ conexion.php
@@ -1,11 +1,18 @@
 <?php
+$sqlp = "CREATE TABLE Tarea(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(30) NOT NULL,
+    descripcion TEXT NOT NULL,
+    fecha TIMESTAMP NOT NULL UNIQUE
+)";
 session_start();
 $conexion = mysqli_connect(
-    'localhost',
-    'root',
-    'Kervisvasquez1993',
-    'phpsql'
+    'postgres://etsoarkokaxwiz:5a5fa9782f4bbb0e6ba3acdd5ee4e595dce5158627e42961c0ea080892cb7dbc@ec2-3-233-100-43.compute-1.amazonaws.com:5432/dd4lvfi0knmca1',
+    'etsoarkokaxwiz',
+    '5a5fa9782f4bbb0e6ba3acdd5ee4e595dce5158627e42961c0ea080892cb7dbc',
+    $sqlp
 );
+mysql_set_charset('utf8', $conexion);
 //if (isset($conexion)) {
 //    echo 'Conexiòn establecida <br>';
 //}
