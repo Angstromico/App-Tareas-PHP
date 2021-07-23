9ef0f0e48eb6354723552151d5af2eaa401b9357
Manuel Esteban Morales Zuarez <79806775+Angstromico@users.noreply.github.com>
2021-07-23 01:02:21 -0400

    Add files via upload

--- conexion.php
+++ conexion.php
@@ -1,8 +1,10 @@
 <?php
 session_start();
-$conn_string = "host=ec2-3-233-100-43.compute-1.amazonaws.com port=5432 dbname=postgresql-angular-05318 user=etsoarkokaxwiz password=5a5fa9782f4bbb0e6ba3acdd5ee4e595dce5158627e42961c0ea080892cb7dbc";
-$conexion = pg_pconnect(
-    $conn_string;
+$conexion = mysqli_connect(
+    'localhost',
+    'root',
+    'Kervisvasquez1993',
+    'phpsql'
 );
 //if (isset($conexion)) {
 //    echo 'Conexiòn establecida <br>';
