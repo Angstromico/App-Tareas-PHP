f8e46fd96b93be5dc3b02b1c60d6d8f6477ba2af
Manuel Esteban Morales Zuarez <79806775+Angstromico@users.noreply.github.com>
2021-07-23 00:42:09 -0400

    Add files via upload

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$conn_string = "host=ec2-3-233-100-43.compute-1.amazonaws.com port=5432 dbname=dd4lvfi0knmca1 user=etsoarkokaxwiz password=5a5fa9782f4bbb0e6ba3acdd5ee4e595dce5158627e42961c0ea080892cb7dbc";
+$conn_string = "host=ec2-3-233-100-43.compute-1.amazonaws.com port=5432 dbname=postgresql-angular-05318 user=etsoarkokaxwiz password=5a5fa9782f4bbb0e6ba3acdd5ee4e595dce5158627e42961c0ea080892cb7dbc";
 $conexion = pg_pconnect(
     $conn_string;
 );
