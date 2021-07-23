e562ec4f2cfa4d7893b0fcd16e733266d4699702
Manuel Esteban Morales Zuarez <79806775+Angstromico@users.noreply.github.com>
2021-07-23 14:40:30 -0400

    Add files via upload

--- save.php
+++ save.php
@@ -1,13 +1,13 @@
 <?php
     include('conexion.php');
     if (isset($_POST['salvar_tarea'])) {
-        $titulo = $_POST['titulo'];
-        $descripcion = $_POST['descripcion'];
+        $titulo = pg_escape_string($_POST['titulo']);
+        $descripcion = pg_escape_string($_POST['descripcion']);
         echo $titulo. '<br>';
         echo $descripcion. '<br>';
         $insercion = "INSERT INTO Tarea (titulo, descripcion) VALUES ('$titulo', '$descripcion')";
         $resultado = pg_query($conexion, $insercion);
-        if (!$resultado) {
+        if ($resultado) {
             //echo 'Exito <br>';
             $_SESSION['message'] = 'Tarea Guardada';
             $_SESSION['message_type'] = 'success';
