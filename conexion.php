c0df654584c7840df9c479c0689fee41c90afcc4
Manuel Esteban Morales Zuarez <79806775+Angstromico@users.noreply.github.com>
2021-07-23 11:47:25 -0400

    Add files via upload

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@ session_start();
 $host = "ec2-3-233-100-43.compute-1.amazonaws.com";
 $username = "etsoarkokaxwiz";
 $password = "5a5fa9782f4bbb0e6ba3acdd5ee4e595dce5158627e42961c0ea080892cb7dbc";
-$database = "postgresql-angular-05318";
+$database = "postgres://etsoarkokaxwiz:5a5fa9782f4bbb0e6ba3acdd5ee4e595dce5158627e42961c0ea080892cb7dbc@ec2-3-233-100-43.compute-1.amazonaws.com:5432/dd4lvfi0knmca1";
 $port = "5432";
 
 $conexion = pg_connect("host=".$host." port=".$port
