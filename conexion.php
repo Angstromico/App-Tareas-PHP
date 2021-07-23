2230302edd6b81d870de5fbb18e772a4304c9c48
Manuel Esteban Morales Zuarez <79806775+Angstromico@users.noreply.github.com>
2021-07-23 00:04:25 -0400

    Add files via upload

--- conexion.php
+++ conexion.php
@@ -1,16 +1,10 @@
 <?php
-$sqlp = "CREATE TABLE Tarea(
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    titulo VARCHAR(30) NOT NULL,
-    descripcion TEXT NOT NULL,
-    fecha TIMESTAMP NOT NULL UNIQUE
-)";
 session_start();
 $conexion = mysqli_connect(
     'postgresql-angular-05318',
     'etsoarkokaxwiz',
     '5a5fa9782f4bbb0e6ba3acdd5ee4e595dce5158627e42961c0ea080892cb7dbc',
-    $sqlp
+    'Tarea'
 );
 //if (isset($conexion)) {
 //    echo 'Conexiòn establecida <br>';
