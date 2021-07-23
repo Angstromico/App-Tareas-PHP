f5e68c79f0201201be3d7aecec83e8122624a68d
Manuel Esteban Morales Zuarez <79806775+Angstromico@users.noreply.github.com>
2021-07-23 00:28:23 -0400

    Add files via upload

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$conexion = mysqli_connect(
+$conexion = pg_connect(
     'postgresql-angular-05318',
     'etsoarkokaxwiz',
     '5a5fa9782f4bbb0e6ba3acdd5ee4e595dce5158627e42961c0ea080892cb7dbc',
