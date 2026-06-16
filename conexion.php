99119c4489d191aceed35003a37f1cd3abc1c002
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-06-16 12:11:04 -0400

    refactor(db): adaptar conexion para usar variables de entorno y soportar docker

--- conexion.php
+++ conexion.php
@@ -1,14 +1,11 @@
 <?php
 session_start();
-$host = "ec2-3-233-100-43.compute-1.amazonaws.com";
-$username = "etsoarkokaxwiz";
-$password = "5a5fa9782f4bbb0e6ba3acdd5ee4e595dce5158627e42961c0ea080892cb7dbc";
-$database = "dd4lvfi0knmca1";
-$port = "5432";
 
-$conexion = pg_connect("host=".$host." port=".$port
-          ." dbname=".$database." user=".$username." password=".$password)
+$host = getenv('DB_HOST') ?: "ec2-3-233-100-43.compute-1.amazonaws.com";
+$username = getenv('DB_USER') ?: "etsoarkokaxwiz";
+$password = getenv('DB_PASSWORD') ?: "5a5fa9782f4bbb0e6ba3acdd5ee4e595dce5158627e42961c0ea080892cb7dbc";
+$database = getenv('DB_NAME') ?: "dd4lvfi0knmca1";
+$port = getenv('DB_PORT') ?: "5432";
+
+$conexion = pg_connect("host=".$host." port=".$port." dbname=".$database." user=".$username." password=".$password)
 or die('No se puedo conectar: ' . pg_last_error());
-//if (isset($conexion)) {
-//    echo 'Conexiòn establecida <br>';
-//}
